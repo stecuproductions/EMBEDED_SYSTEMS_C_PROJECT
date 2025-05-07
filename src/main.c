38bc1301634aa0f8e7c5a09c7792b05622d1ce89
Jakub Stec <stecu03@gmail.com>
2025-05-07 10:59:21 +0200

    Muzyczka

--- src/main.c
+++ src/main.c
@@ -128,7 +128,58 @@ double calculateAverageScore(double *arr) {
 
 //music
 
+static uint32_t notes[] = {
+    2272, // A - 440 Hz
+    2024, // B - 494 Hz
+    3816, // C - 262 Hz
+    3401, // D - 294 Hz
+    3030, // E - 330 Hz
+    2865, // F - 349 Hz
+    2551, // G - 392 Hz
+    1136, // a - 880 Hz
+    1012, // b - 988 Hz
+    1912, // c - 523 Hz
+    1703, // d - 587 Hz
+    1517, // e - 659 Hz
+    1432, // f - 698 Hz
+    1275, // g - 784 Hz
+};
 
+uint32_t getNote(char ch) {
+    if (ch >= 'A' && ch <= 'G')
+        return notes[ch - 'A'];
+    if (ch >= 'a' && ch <= 'g')
+        return notes[ch - 'a' + 7];
+    return 0;
+}
+
+void playNote(uint32_t note, uint32_t durationMs) {
+    uint32_t t = 0;
+    while (t < durationMs * 1000) {
+        NOTE_PIN_HIGH();
+        Timer0_Wait(note / 2);
+        NOTE_PIN_LOW();
+        Timer0_Wait(note / 2);
+        t += note;
+    }
+}
+
+
+void playMySong(void) {
+    const char* melody = "C4,E4,G4,C5,";   //C 400 S E 400 S G 400 S ITD...
+
+    while (*melody != '\0') {
+        uint32_t note = getNote(*melody++); //Czytamy znak (C i posinkrementujemy)
+        uint32_t duration = 400;  //duration (losowa liczba, zaraz ja ustawimy)
+
+        if (*melody >= '0' && *melody <= '9') {
+            duration = (*melody++ - '0') * 100;  // n '4' = 400 ms
+        }
+
+        playNote(note, duration);
+        Timer0_Wait(50);
+    }
+}
 
 //Screen display functions
 
